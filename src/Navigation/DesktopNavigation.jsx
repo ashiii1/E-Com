1948fd1a91e305f56b8e0d7df18e03992bd3d2c6
ashiii1 <aashishaik526@gmail.com>
2024-05-27 21:40:20 +0530

    navigator res

--- src/Navigation/DesktopNavigation.jsx
+++ src/Navigation/DesktopNavigation.jsx
@@ -27,7 +27,7 @@ const DesktopNavigation = () => {
       <nav className='nav'>
         <div className="logo">
           <Link to='/'>
-            <span >Shop It</span>
+            <span >All In One</span>
           </Link>
         </div>
         <div className="nav-items">
